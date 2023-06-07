8eb1f8acb9036a63576965c13f1d95370501e52b
akohinmiogbegbile <akohinmiogbegbile>
2023-06-06 18:19:47 -0700

    Conditional loops in python

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    print('{:c}'.format(i), end='')
+# 2-print_alphabet.py
+
+"""Print the alphabet in lowercase, not followed by a new line."""
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
