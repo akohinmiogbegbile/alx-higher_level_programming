3421b805a9ffe372e59fbf2e5bb91945cc8cb7c1
akohinmiogbegbile <akohinmi90@gmail.com>
2023-09-29 19:50:21 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-# a Python script that fetches https://alx-intranet.hbtn.io/status using urllib
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
+
 
 if __name__ == '__main__':
     import urllib.request
@@ -10,4 +14,3 @@ if __name__ == '__main__':
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode('utf-8')))
-
